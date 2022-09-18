453dade3c73fedc4f956116a3fcea7539a171e87
Emotions <estskywalker@gmail.com>
2022-09-19 00:25:27 +0700

    change scripts

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const { google } = require("googleapis");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+});
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
